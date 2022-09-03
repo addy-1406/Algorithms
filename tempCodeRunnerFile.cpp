a922a8a7c1aee4b30b5768494e3140c6a18f9d14
Aditya Mandal <adityamandal140603@gmail.com>
2022-09-03 13:03:37 +0530

    Initial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+void heapSort(int arr[], int n){
+//     buildHeap(arr,n);
+//     int last = n;
+//     for(int i = n; i > 1; i--){
+//         swap(arr[i],arr[1]);
+//         heapify(arr,i-1);
+//     }
+//     return;
+// }
